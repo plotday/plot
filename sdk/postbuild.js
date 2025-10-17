ce23ab26d5db58c7c6ef8ab890938110f783ba6f
Kris Braun <kris.braun@gmail.com>
2025-10-17 17:57:26 -0400

    SDK 0.9.0 release

--- sdk/postbuild.js
+++ sdk/postbuild.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import { writeFileSync, chmodSync } from "fs";
-import { fileURLToPath } from "url";
+import { chmodSync, writeFileSync } from "fs";
 import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Create bin/package.json to mark the directory as CommonJS
 const binPackageJson = {
-  type: "commonjs"
+  type: "commonjs",
 };
 const binPackageJsonPath = join(__dirname, "bin", "package.json");
 writeFileSync(binPackageJsonPath, JSON.stringify(binPackageJson, null, 2));
@@ -23,4 +23,4 @@ const binPath = join(__dirname, "bin", "plot.cjs");
 writeFileSync(binPath, wrapperContent);
 chmodSync(binPath, 0o755);
 
-console.log("Created CLI wrapper at bin/plot.cjs");
+console.log("✓ Built CLI");
