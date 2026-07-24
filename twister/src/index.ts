4fadd2fe6601ce9fb5fdc46d6f12740b36820871
Kris Braun <kris.braun@gmail.com>
2026-07-23 21:52:19 -0400

    feat(messaging): add Gmail-scoped canonicalizeEmail and baseEmail

--- twister/src/index.ts
+++ twister/src/index.ts
@@ -14,6 +14,7 @@ export {
 } from "./drain";
 export * from "./tools";
 export * from "./messaging/reply-recipients";
+export * from "./messaging/canonical-email";
 export * from "./options";
 export * from "./utils/types";
 export { getBuilderDocumentation } from "./creator-docs";
